ea9fb0e36982de8f740c1eba7a47a33850ee70dd
valeriecollinsdesign <valeriecollins270@gmail.com>
2022-06-04 18:46:23 -0600

    implementing jQuery
    animated nav bar added hamburger menu. added back to top links and project links. fixed header in place added a minor shadow and a bit of a blur. checked colors for accessibility.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 console.log("Hello friend! Glad you could stop by");
 
+
+
+/*menu toggle*/
+function menuToggle(x) {
+	var x = document.getElementById('myNavtoggle');
+	if (x.className === 'navtoggle') {
+	  x.className += ' responsive';
+	} else {
+	  x.className = 'navtoggle';
+	}
+
+  }
+$('#click_advance').click(function() {
+	$('#display_advance').toggle('1000');
+	$("i", this).toggleClass("fas fa-bars fas fa-x");
+});
+
 $('.nav-link').on('click', function() {
 	$('.active-link').removeClass('active-link');
 	$(this).addClass('active-link');
-});
+});/* jquery for the nav animation to work*/
