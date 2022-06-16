c5809fe95c6736104bcb6075fed51bb933932e39
valeriecollinsdesign <valeriecollins270@gmail.com>
2022-06-16 14:10:38 -0600

    Fixing errors and adding buttons
    Fixed padding on all pages, added sticky back to top button, added links to other pages, added work button in top nav.

--- js/script.js
+++ js/script.js
@@ -21,3 +21,19 @@ $('.nav-link').on('click', function() {
 	$('.active-link').removeClass('active-link');
 	$(this).addClass('active-link');
 });/* jquery for the nav animation to work*/
+
+/*back to top button*/
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
